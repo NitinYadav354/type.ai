81cbe37065cd89aff12bf045ad205d759611887e
Nitin Yadav <nyadav4625@gmail.com>
2026-06-16 00:37:32 +0530

    feat: Add axios service for ai API call

---
+++ type-frontend/src/Services/AiCoachAPI.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import type { OptimisedKeystroke } from "../Utility/optimseKeystroke";
+
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+export const fetchAiCoachResponse = async (optimisedKeystrokes: OptimisedKeystroke[]) => {
+    const response = await axios.post(`${API_BASE_URL}/api/coach`, { keystrokes: optimisedKeystrokes });
+    return response.data;
+}
