c44cbec6a3f346690e5f9d9611d1593add26cf9d
Nitin Yadav <nyadav4625@gmail.com>
2026-09-22 01:48:47 +0530

    feat(dashboard): update dashboard component adding Avg WMP and Avg Accuracy and total time

--- type-frontend/src/Components/Dashboard.tsx
+++ type-frontend/src/Components/Dashboard.tsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from 'react';
 import { fetchDashboardData } from '../Services/DashboardAPI';
+import StatCard from './StatCard';
+
+const formatTime = (totalSeconds: number) => {
+    if (!totalSeconds) return '0h 0m';
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    return `${hours}h ${minutes}m`;
+};
 
 export default function Dashboard() {
     const [data, setData] = useState<any>(null);
@@ -58,16 +66,22 @@ export default function Dashboard() {
     }
 
     return (
-        <div className="dashboard-container" style={{ marginTop: '40px', padding: '20px', backgroundColor: '#1E1E2E', borderRadius: '12px' }}>
-            <h2 style={{ color: '#818CF8', marginBottom: '20px' }}>Your Statistics</h2>
+        <div className="dashboard-container" style={{ width: '100%', marginTop: '20px', padding: '20px', backgroundColor: '#1E1E2E', borderRadius: '12px' }}>
+            <h2 style={{ color: '#818CF8', marginBottom: '24px' }}>Your Statistics</h2>
             
-            <div style={{ color: '#A6ACCD', fontSize: '14px', backgroundColor: '#11111B', padding: '15px', borderRadius: '8px' }}>
-                <p>✅ Data successfully fetched!</p>
-                <p>Total Tests: {data?.summary?.totalTests}</p>
-                <p>Personal Best (WPM): {data?.summary?.maxWpm}</p>
+            <div style={{
+                display: 'grid',
+                gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+                gap: '16px',
+                marginBottom: '40px'
+            }}>
+                <StatCard title="Total Tests" value={data?.summary?.totalTests || 0} />
+                <StatCard title="Total Time" value={formatTime(data?.summary?.totalTimeSeconds)} />
+                <StatCard title="Personal Best" value={data?.summary?.maxWpm || 0} subtitle="Words Per Minute" />
+                <StatCard title="Avg WPM" value={data?.summary?.avgWpm || 0}/>
+                <StatCard title="Avg Accuracy" value={`${data?.summary?.avgAccuracy || 0}%`} />
             </div>
             
-            <div id="dashboard-summary-cards" style={{ marginTop: '20px' }}></div>
             <div id="dashboard-line-chart" style={{ marginTop: '20px' }}></div>
             <div id="dashboard-heatmap" style={{ marginTop: '20px' }}></div>
         </div>
